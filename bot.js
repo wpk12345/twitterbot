5969a74ec275d9356da739f1f976e5348fd0a548
William King <wpk12345@gmail.com>
2017-10-19 13:55:36 -0400

    second commit

---
+++ bot.js
@@ -0,0 +1,6 @@
+console.log('The bot is starting!');
+
+var Twit = require('twit');
+var config = require('./config');
+
+var T = new Twit(config);
---
+++ config.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+
+    consumer_key:         'Wu8mUzdfmVWgRYVxbG6pJLOAl',
+    consumer_secret:      '	EpFYjXETydYHk2dz5Fhpy2MrZiAqgTw8iOtSLkK1TvMZ4EzXnk',
+    access_token:         '	920774463301570561-1yq8GA4cqIImYqPtCrVgMuOHrqHdV5d',
+    access_token_secret:  'UQajIlTUdG6pcTGcP6UMs0AMwqWEpBvaNvKSNabFEYpE7',
+    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests. 
+}
